363c795ddae199089101a7bd3d8ba1d7ce11ea20
Thinh Pham <phamvanthinh2001x@gmail.com>
2021-08-27 23:55:41 +0700

    test setting prettier

--- src/App.js
+++ src/App.js
@@ -4,39 +4,46 @@ import Album from './features/Album';
 import { NavLink, Redirect, Route, Switch, useHistory, useLocation, useParams, useRouteMatch } from 'react-router-dom';
 import NotFound from './components/NotFound';
 
-
 function App() {
   const Thinh = {
     name: 'Pham Van Thinh',
     age: '20',
     male: true,
-  }
+  };
   const history = useHistory();
-  console.log(history)
-  
+  console.log(history);
+
   const location = useLocation();
-  console.log(location)
-  
+  console.log(location);
+
   const params = useParams();
-  console.log(params)
+  console.log(params);
 
   const rou = useRouteMatch();
-  console.log(rou)
+  console.log(rou);
 
   return (
     <div className="App">
       <p className="header">Header</p>
-      <p className="nav__link"><NavLink to='/' exact>Home</NavLink></p>
-      <p className="nav__link"><NavLink to='/todos'>Todos</NavLink></p>
-      <p className="nav__link"><NavLink to='/album'>Album</NavLink></p>
+      <p className="nav__link">
+        <NavLink to="/" exact>
+          Home
+        </NavLink>
+      </p>
+      <p className="nav__link">
+        <NavLink to="/todos">Todos</NavLink>
+      </p>
+      <p className="nav__link">
+        <NavLink to="/album">Album</NavLink>
+      </p>
 
       <Switch>
         <Redirect from="/home" to="/" exact />
         <Redirect from="/post-list/:postId" to="/posts/:postId" exact />
 
-        <Route path='/' component ={TodoFeatures} exact />
-        <Route path='/todos' component={TodoFeatures} />
-        <Route path='/album' component={Album} />
+        <Route path="/" component={TodoFeatures} exact />
+        <Route path="/todos" component={TodoFeatures} />
+        <Route path="/album" component={Album} />
         <Route component={NotFound} />
       </Switch>
       <p className="footer">Footer</p>
--- src/features/Todo/pages/ListPage/index.jsx
+++ src/features/Todo/pages/ListPage/index.jsx
@@ -2,12 +2,10 @@ import React, { useEffect, useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 import TodoList from '../../components/TodoList';
 import './styles.scss';
-import { useHistory, useLocation, useParams, useRouteMatch } from "react-router-dom";
+import { useHistory, useLocation, useParams, useRouteMatch } from 'react-router-dom';
 import queryString, { stringify } from 'query-string';
 
-ListPage.propTypes = {
-    
-};
+ListPage.propTypes = {};
 
 function ListPage(props) {
     const initTodoList = [
@@ -25,111 +23,118 @@ function ListPage(props) {
             id: 3,
             title: 'code',
             status: 'new',
-        }
-    ]
+        },
+    ];
 
     const history = useHistory();
-    console.log(history)
-    
+    console.log(history);
+
     const locatio = useLocation();
-    console.log(locatio)
-    
+    console.log(locatio);
+
     const params = useParams();
-    console.log(params)
+    console.log(params);
 
     const rou = useRouteMatch();
-    console.log(rou)
-    
+    console.log(rou);
+
     const location = useLocation();
     const match = useRouteMatch();
-    const [todoList, setTodoList] = useState(initTodoList)
+    const [todoList, setTodoList] = useState(initTodoList);
     const [filterStatus, setFilterStatus] = useState(() => {
-        return queryString.parse(location.search).status || 'all'
-    })
+        return queryString.parse(location.search).status || 'all';
+    });
 
     useEffect(() => {
-        setFilterStatus(queryString.parse(location.search).status || 'all')
-    }, [location.search])
+        setFilterStatus(queryString.parse(location.search).status || 'all');
+    }, [location.search]);
 
     const onTodoClick = (todo, index) => {
         const newTodoList = [...todoList];
         newTodoList[index] = {
             ...newTodoList[index],
-            status: newTodoList[index].status == 'new' ? 'completed' : 'new'
-        }
+            status: newTodoList[index].status == 'new' ? 'completed' : 'new',
+        };
         setTodoList(newTodoList);
-    }
+    };
 
     const removeClick = (todo, index) => {
         const newTodoList = [...todoList];
         newTodoList.splice(index, 1);
         setTodoList(newTodoList);
-    }
+    };
 
     const handleShowAllClick = () => {
         // setFilterStatus('all')
-        const queryParams = {status: 'all'}
+        const queryParams = { status: 'all' };
         history.push({
             pathname: match.path,
-            search: queryString.stringify(queryParams)
-        })
-    }
+            search: queryString.stringify(queryParams),
+        });
+    };
     const handleShowNewClick = () => {
         // setFilterStatus('new')
-        const queryParams = {status: 'new'}
+        const queryParams = { status: 'new' };
         history.push({
             pathname: match.path,
-            search: queryString.stringify(queryParams)
-        })
-    }
+            search: queryString.stringify(queryParams),
+        });
+    };
     const handleShowCompletedClick = () => {
         // setFilterStatus('completed')
-        const queryParams = {status: 'completed'}
+        const queryParams = { status: 'completed' };
         history.push({
             pathname: match.path,
-            search: queryString.stringify(queryParams)
-        })
-    }
+            search: queryString.stringify(queryParams),
+        });
+    };
 
     const setId = (todoList) => {
         let id = todoList.length;
-        for(var i = 0; i < todoList.length; i++){
-            if(id === todoList[i].id){
+        for (var i = 0; i < todoList.length; i++) {
+            if (id === todoList[i].id) {
                 id = id + 1;
                 i = 0;
             }
         }
         return id;
-    }
+    };
     const addItem = () => {
         const value = document.querySelector('.todo__input').value;
-        if(value === '') return;
+        if (value === '') return;
         const newTodo = {
             id: setId(todoList),
             title: value,
             status: 'new',
-        }
+        };
         const newTodoList = [...todoList, newTodo];
         setTodoList(newTodoList);
         console.log(todoList);
         document.querySelector('.todo__input').value = '';
-    }
+    };
 
     const renderedTodoList = useMemo(() => {
-        return todoList.filter(todo => filterStatus === 'all' || filterStatus === todo.status)
-    }, [todoList, filterStatus]) 
-
+        return todoList.filter((todo) => filterStatus === 'all' || filterStatus === todo.status);
+    }, [todoList, filterStatus]);
 
     return (
         <div className="todo">
             <h3 className="todo__title">Todo List</h3>
-            <TodoList todoList={renderedTodoList} onTodoClick={onTodoClick} removeClick={removeClick}/>
-            <input style={{display: 'block'}} className="todo__input" type="text" placeholder="Add Item"></input>
-            <button style={{display: 'block', cursor: "pointer"}} onClick={addItem} className="todo__btn-add">Add</button>
+            <TodoList todoList={renderedTodoList} onTodoClick={onTodoClick} removeClick={removeClick} />
+            <input style={{ display: 'block' }} className="todo__input" type="text" placeholder="Add Item"></input>
+            <button style={{ display: 'block', cursor: 'pointer' }} onClick={addItem} className="todo__btn-add">
+                Add
+            </button>
             <div>
-                <button className="todo__btn-show" onClick={handleShowAllClick}>Show All</button>
-                <button className="todo__btn-show" onClick={handleShowNewClick}>Show New</button>
-                <button className="todo__btn-show" onClick={handleShowCompletedClick}>Show Completed</button>
+                <button className="todo__btn-show" onClick={handleShowAllClick}>
+                    Show All
+                </button>
+                <button className="todo__btn-show" onClick={handleShowNewClick}>
+                    Show New
+                </button>
+                <button className="todo__btn-show" onClick={handleShowCompletedClick}>
+                    Show Completed
+                </button>
             </div>
         </div>
     );
