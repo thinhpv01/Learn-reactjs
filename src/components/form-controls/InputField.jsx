2880699da2e5d92bce9f5f202076be3436698fe5
Thinh Pham <phamvanthinh2001x@gmail.com>
2021-08-29 15:45:39 +0700

    ...

--- src/components/form-controls/InputField.jsx
+++ src/components/form-controls/InputField.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { TextField } from '@material-ui/core';
 import { Controller } from 'react-hook-form';
+
 InputField.propTypes = {
     form: PropTypes.object.isRequired,
     name: PropTypes.string.isRequired,
--- src/features/Todo/components/TodoForm/index.jsx
+++ src/features/Todo/components/TodoForm/index.jsx
@@ -1,10 +1,9 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-// import InputField from '../../../../components/form-controls/InputField';
-import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
+import InputField from 'components/form-controls/InputField';
+import PropTypes from 'prop-types';
+import React from 'react';
+import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
-import InputField from '../../../../components/form-controls/InputField';
 TodoForm.propTypes = {
     onSubmit: PropTypes.func,
 };
