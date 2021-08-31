db19aebec1fd65bd431eafe97ce6e013a3862e46
Thinh Pham <phamvanthinh2001x@gmail.com>
2021-08-31 16:40:18 +0700

    set api dang ky

--- src/features/Auth/Component/Register/index.jsx
+++ src/features/Auth/Component/Register/index.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import RegisterForm from '../RegisterForm';
+import { useDispatch } from 'react-redux';
+import { register } from 'features/Auth/userSlice';
 
 Register.propTypes = {};
 
 function Register(props) {
-    const handleSubmit = (values) => {
-        console.log(values);
+    const dispatch = useDispatch();
+    const handleSubmit = async (values) => {
+        try {
+            values.username = values.email;
+            console.log(values);
+            const action = register(values);
+            const user = await dispatch(action).unwrap();
+            console.log(user);
+        } catch (error) {
+            console.log('Failed to register: ', error);
+        }
     };
     return <RegisterForm onSubmit={handleSubmit} />;
 }
